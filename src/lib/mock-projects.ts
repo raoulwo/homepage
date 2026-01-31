9d66fa1c2afa12e8a84d10959645225c2dddb4f6
Raoul Wograndl <wograndl.raoul@gmail.com>
2026-01-31 17:08:46 +0100

    refactor: create project placeholder

---
+++ src/lib/mock-projects.ts
@@ -0,0 +1,24 @@
+import type { Project } from "../types/project.ts";
+
+export const projects: Project[] = [
+  {
+    title: "My first project",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores, at corporis dolor ducimus excepturi, magnam quaerat quidem quo quos reiciendis tempora tempore temporibus? At commodi eligendi modi obcaecati veniam?",
+  },
+  {
+    title: "My second project",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores, at corporis dolor ducimus excepturi, magnam quaerat quidem quo quos reiciendis tempora tempore temporibus? At commodi eligendi modi obcaecati veniam?",
+  },
+  {
+    title: "My third project",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores, at corporis dolor ducimus excepturi, magnam quaerat quidem quo quos reiciendis tempora tempore temporibus? At commodi eligendi modi obcaecati veniam?",
+  },
+  {
+    title: "My fourth project",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores, at corporis dolor ducimus excepturi, magnam quaerat quidem quo quos reiciendis tempora tempore temporibus? At commodi eligendi modi obcaecati veniam?",
+  },
+];
